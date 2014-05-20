9ad5df4e73f95d18d2e806040f70c7942a3010e3
sisk0 <sisko.bg@gmail.com>
2014-05-20 15:03:20 +0300

    Create SomeOtherStuffs for Screen.java

---
+++ JavaIntroTeamProject/src/SomeOtherStuffs for Screen.java
@@ -0,0 +1,47 @@
+public void paint(Graphics g) {
+		g.clearRect(0, 0, WIDTH, HEIGHT);
+		
+		g.setColor(new Color(10, 50, 0));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		g.setColor(Color.BLACK);
+		for(int i = 0; i < WIDTH / 10; i++) {
+			g.drawLine(i * 10, 0, i * 10, HEIGHT);
+		}
+		
+		for(int i = 0; i < HEIGHT / 10; i++) {
+			g.drawLine(0, i * 10, WIDTH, i * 10);
+		}
+		
+		for(int i = 0; i < snake.size(); i++) {
+			snake.get(i).draw(g);
+		}
+		for(int i = 0; i < apples.size(); i++) {
+			apples.get(i).draw(g);
+		}
+	
+	}
+	
+	public void start() {
+		running = true;
+		thread = new Thread(this, "Game Restart");
+		thread.start();
+	}
+	
+	public void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while(running) {
+			tick();
+			repaint();
+		}
+	}
+	
