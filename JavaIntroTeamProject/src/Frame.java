2ee34a72034cf1f07f0703295d2d15337c5cc9ec
sisk0 <sisko.bg@gmail.com>
2014-05-20 14:59:27 +0300

    Create Frame.java

---
+++ JavaIntroTeamProject/src/Frame.java
@@ -0,0 +1,27 @@
+public class Frame extends JFrame {
+
+	public Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Snake");
+		setResizable(false);
+		
+		init();
+	}
+	
+	public void init() {
+		setLayout(new GridLayout(1, 1, 0, 0));
+		
+		Screen s = new Screen();
+		add(s);
+		
+		pack();
+		
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Frame();
+	}
+	
+}
